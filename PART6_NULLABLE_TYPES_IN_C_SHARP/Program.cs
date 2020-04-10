19f93df30bcef71a460fc588c653ec5eac2a390c
jbutler <jbutler@caroffer.com>
2020-04-10 00:38:41 -0500

    3rd Part 6 commit

--- PART6_NULLABLE_TYPES_IN_C_SHARP/Program.cs
+++ PART6_NULLABLE_TYPES_IN_C_SHARP/Program.cs
@@ -10,7 +10,20 @@ namespace PART6_NULLABLE_TYPES_IN_C_SHARP
     {
         static void Main(string[] args)
         {
+            bool? AreYouMajor = null;
 
+            if(AreYouMajor == true)
+            {
+                Console.WriteLine("User is Major");
+            }
+            else if (!AreYouMajor.Value)
+            {
+                Console.WriteLine("User is not Major");
+            }
+            else
+            {
+                Console.WriteLine("User did not answer the question");
+            }
         }
     }
 }
